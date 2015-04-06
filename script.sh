80b70df612a0e7dd797b0b10a3c64322fb2d20f0
Dima-Git <urmemoris@gmail.com>
2015-04-06 21:09:33 +0300

    Added test script.

---
+++ script.sh
@@ -0,0 +1,20 @@
+sudo insmod timer_sysfs.ko
+
+echo "1sec timeout, 3sec sleep"
+echo 1000 > /sys/timer_dir/timer_period
+sleep 3
+
+echo "0.5sec timeout, 3sec sleep"
+echo 500 > /sys/timer_dir/timer_period
+sleep 3
+
+echo "0sec timeout, 3sec sleep"
+echo 0 > /sys/timer_dir/timer_period
+sleep 3
+
+echo "2sec timeout, 3sec sleep"
+echo 2000 > /sys/timer_dir/timer_period
+sleep 3
+
+sudo rmmod timer_sysfs
+
